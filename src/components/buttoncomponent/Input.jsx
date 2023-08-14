099f225eea9e6cbc79c44420f18efaa50d59e515
Layaraia <fazanka0000000000@mail.ru>
2023-08-14 17:06:12 +0800

    css some work

--- src/components/buttoncomponent/Input.jsx
+++ src/components/buttoncomponent/Input.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "../../styles/buttonComponent/Input.css";
 
 const Input = ({ setName }) => {
-  return <input type="text" onChange={(e) => setName(e.target.value)} />;
+  return (
+  <input 
+  className="inputText"
+  type="text" 
+  onChange={(e) => setName(e.target.value)} 
+  />
+  )
 };
 
 export default Input;
--- src/components/buttoncomponent/TextAfterInput.jsx
+++ src/components/buttoncomponent/TextAfterInput.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../../styles/buttonComponent/TextAfterInput.css";
 
 const TextAfterInput = ({ showName }) => {
   return <div className="textAfterInput">Невероятно! Вы - {showName}</div>;
