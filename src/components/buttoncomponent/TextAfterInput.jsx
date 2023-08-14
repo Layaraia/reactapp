55048abd60055c6a791ac40255021dcda196210d
Layaraia <fazanka0000000000@mail.ru>
2023-08-14 17:25:04 +0800

    bim bim bam bam

--- src/components/buttoncomponent/TextAfterInput.jsx
+++ src/components/buttoncomponent/TextAfterInput.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../../styles/buttonComponent/TextAfterInput.css";
 
 const TextAfterInput = ({ showName }) => {
-  return <div className="textAfterInput">Невероятно! Вы - {showName}</div>;
+  return <div className="textAfterInput">Невероятно! Вы - <span>{showName}</span></div>;
 };
 
 export default TextAfterInput;
