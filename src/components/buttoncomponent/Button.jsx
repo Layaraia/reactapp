734c9162211a44b8ef3c44884409f78ebebf5dfb
Layaraia <fazanka0000000000@mail.ru>
2023-08-14 17:54:45 +0800

    bum bum bim

--- src/components/buttoncomponent/Button.jsx
+++ src/components/buttoncomponent/Button.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "../../styles/buttonComponent/Button.css";
 
 const Button = ({ name, setShowName }) => {
-  return <button onClick={() => setShowName(name)}>Предсказать!</button>;
+  return <button id="superbutton" onClick={() => setShowName(name)}>Предсказать!</button>;
 };
 
 export default Button;
