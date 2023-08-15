db8594fc4071a85775a861aa492c75b0e7f1cc76
Layaraia <fazanka0000000000@mail.ru>
2023-08-15 19:09:24 +0800

    add cool mage image

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import "./global.css";
 function App() {
   return (
     <div className="container">
+      <div>
+        <img scr='/Mage.webp' alt='cool mage' />
+      </div>
+      <div>
       <Welcome />
       <ButtonWithText />
       {/*{NAV.map(item=>(<div>{item.title}</div>))}*/}
+      </div>
     </div>
   );
 }
