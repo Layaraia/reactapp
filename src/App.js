6aa83c15c051419a895be6a46f35a4461866d018
Layaraia <fazanka0000000000@mail.ru>
2023-08-15 21:27:44 +0800

    craco

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import Welcome from "./components/Welcome";
 import ButtonWithText from "./components/buttoncomponent/ButtonWithText";
 /*import { NAV } from "./const/nav";*/
 import "./global.css";
+import mageImage from "./image/mage.png";
 
 function App() {
   return (
     <div className="container">
       <div>
-        <img src="/image/mage.png" alt="cool mage" />
+        <img src={mageImage} alt="cool mage" />
       </div>
       <div>
         <Welcome />
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { MAGE } from "../const/MAGE";
 
 const Welcome = () => {
   return (
-    <div>
       <div>
-      <img src={MAGE} alt="cool mage" />
-      </div>
-      <div>
-        <h1>Вы посетили сайт Загадочной Гадалки!</h1>
+        <h1>Вы посетили сайт Великолепного Пресказателя!</h1>
         <h2>Здесь вы можете узнать о себе всю правду.</h2>
         <h3>Введите свое имя в поле ниже</h3>
       </div>
-    </div>
   );
 };
 
